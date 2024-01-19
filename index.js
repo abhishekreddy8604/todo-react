9e7e328653f30610359ea507d03c1ceb4138d865
abhishekreddy8604 <abhishekreddy0000@gmail.com>
2024-01-19 11:28:22 -0500

    updated index file

--- index.js
+++ index.js
@@ -14,20 +14,36 @@ app.use(exoress.json());
 
 //}
 
-app.post("/todo", function (req, res) {
+app.post("/todo", async function (req, res) {
   const createPayload = req.body;
   const parsePayload = createTodo.safeParse(createPayload);
   if (!parsePayload.success) {
     res.status(411).json({
-      msh: "Sorry! wrong inputs",
+      msg: "Sorry! wrong inputs",
     });
     return;
   }
+
+  await todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+
+  experss.response.json({
+    msg: "Todo Created",
+  });
 });
 
-app.get("/todos", function (req, res) {});
+app.get("/todos", async function (req, res) {
+  const todos = await todo.find({});
 
-app.put("/completed", function (req, res) {
+  res.json({
+    msg: "Todo created",
+  });
+});
+
+app.put("/completed", async function (req, res) {
   const updateTodo = req.body;
   const parsePayload = updateTodo.safeParse(updateTodo);
   if (!parsePayload.success) {
@@ -36,4 +52,16 @@ app.put("/completed", function (req, res) {
     });
     return;
   }
+
+  await todo.update(
+    {
+      _id: req.body.id,
+    },
+    {
+      completed: true,
+    }
+  );
+  res.json({
+    msg: "Todo marked as completed",
+  });
 });
